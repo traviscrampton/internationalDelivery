a4a22c0f2c07809c50a57f2dfd39b82ee85e2ed1
Travis Crampton <crampton.travis@gmail.com>
2015-10-29 15:20:53 -0700

    Index Page Working For Requests.item.itemimage

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,20 @@ class ItemsController < ApplicationController
       @item = Item.new
   end
 
+  def create
+    @request = Request.find(params[:request_id])
+    @item = @request.items.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:itemname, :itemdescription, :itemimage)
+  end
+
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,9 @@ class Item < ActiveRecord::Base
   validates :itemname, :presence => true
   validates :itemdescription, :presence => true
 
+  has_attached_file :itemimage, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+validates_attachment_content_type :itemimage, :content_type => /\Aimage\/.*\Z/
+
   belongs_to :request
   belongs_to :answer
 end
